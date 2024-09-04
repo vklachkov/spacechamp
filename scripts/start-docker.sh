3dbffd4a69861a3f7de81c5330345e119971f11c
Kozachenko Diana <kozachenkodiana03@mail.ru>
2024-09-04 11:23:40 +0300

    Деплой

---
+++ scripts/start-docker.sh
@@ -0,0 +1,7 @@
+image_name=space-fest-image;
+container_name=space-fest-cont;
+
+sudo docker stop $container_name
+sudo docker rm $container_name
+sudo docker build . -t $image_name
+sudo docker run -d -p 4200:80 --name $container_name $image_name:latest
