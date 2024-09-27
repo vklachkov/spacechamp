705a77c976bbcc35c8c4017746ac954ac2360cff
Valery Klachkov <zgdump@gmail.com>
2024-09-27 22:18:50 +0300

    feat(report-gen): Исправлена печать в PDF, локальные шрифты

--- report-generator/app.py
+++ report-generator/app.py
@@ -1,9 +1,14 @@
+import os
+import shutil
 from flask import Flask, request, render_template, Response
 from typing import Dict
 import click
 import subprocess
 
 app = Flask(__name__)
+
+report_html_path = ""
+report_pdf_path = ""
 
 @app.route('/', methods=['POST'])
 def report_handler():
@@ -16,24 +21,26 @@ def report_handler():
     return Response(pdf, mimetype='application/pdf')
 
 def get_rate_pages():
-    rates = request.get_json()
+    participants = request.get_json()
     page_size = 15
     pages = []
-    
-    for (i, (code, rate)) in enumerate(rates):
-        participant_idx = i % page_size
 
-        if participant_idx == 0:
+    for (i, participant) in enumerate(participants):
+        idx = i % page_size
+        code = participant['code']
+        rates = participant['rates']
+
+        if idx == 0:
             page_number = (i // page_size) + 1
             page_participants = [{} for _ in range(page_size)]
             pages.append({ 'number': page_number, 'participants': page_participants })
 
-        pages[-1]['participants'][participant_idx] = { 'number': i + 1, 'code': code, 'rate': rate }
-    
+        pages[-1]['participants'][idx] = { 'number': i + 1, 'code': code, 'rates': rates }
+
     return pages
 
 def get_date():
-    from datetime import datetime, timezone
+    from datetime import datetime
     from zoneinfo import ZoneInfo
     
     now = datetime.now(tz=ZoneInfo('Europe/Moscow'))
@@ -45,22 +52,38 @@ def get_date():
     }
 
 def render_html_to_pdf(html):
-    TMP_PAGE_PATH = '/tmp/html4pdf.html'
-    TMP_PDF_PATH = '/tmp/pdf4user.pdf'
-
-    with open(TMP_PAGE_PATH, 'w') as doc:
+    with open(report_html_path, 'w') as doc:
         doc.write(html)
 
     subprocess.run(['chromium',
                     '--headless',
                     '--disable-gpu', '--run-all-compositor-stages-before-draw',
                     '--no-margins', '--no-pdf-header-footer',
-                    '--virtual-time-budget=2000',
-                    f'--print-to-pdf={TMP_PDF_PATH}',
-                    f'file://{TMP_PAGE_PATH}'])
+                    '--virtual-time-budget=1000',
+                    f'--print-to-pdf={report_pdf_path}',
+                    f'file://{report_html_path}'])
     
-    with open(TMP_PDF_PATH, 'rb') as pdf:
+    with open(report_pdf_path, 'rb') as pdf:
         return pdf.read()
+
+def prepare_files():
+    global report_html_path
+    global report_pdf_path
+
+    base = '/tmp/spacechamp-report-generator'
+
+    if os.path.exists(base):
+        shutil.rmtree(base)
+
+    os.mkdir(base)
+
+    shutil.copytree('static/', f'{base}/static')
+    
+    report_html_path = f'{base}/report.html'
+    with open(report_html_path, 'wb') as html: pass
+
+    report_pdf_path = f'{base}/report.pdf'
+    with open(report_pdf_path, 'wb') as pdf: pass
 
 @click.command()
 @click.option('--host', type=click.STRING)
@@ -76,4 +99,5 @@ def start_server(host: str, port: int, debug: bool):
         serve(app, host=host, port=port)
 
 if __name__ == '__main__':
+    prepare_files()
     start_server()
