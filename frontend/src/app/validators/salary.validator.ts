a062d283f349195f95b4ffb2e61ada9323e17206
Valery Klachkov <zgdump@gmail.com>
2024-09-29 17:18:02 +0300

    fix(frontend): Исправлена валидация, разрешена зарплата 0

--- frontend/src/app/validators/salary.validator.ts
+++ frontend/src/app/validators/salary.validator.ts
@@ -4,7 +4,7 @@ export function salaryValidator(min: number, max: number): ValidatorFn {
   return (control: AbstractControl<number | null>): ValidationErrors | null => {
     const value: number | null = control.value;
 
-    if (!value) {
+    if (value === null) {
       return null;
     }
 
